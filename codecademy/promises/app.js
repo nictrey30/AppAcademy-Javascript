c27b1f5c4315a0922edb313bb0dff05ec23f4e7c
Traian <treytong35@gmail.com>
2020-03-18 00:31:12 +0200

    codecademy promises

---
+++ codecademy/promises/app.js
@@ -0,0 +1,8 @@
+const checkInventory = require('./library.js');
+const order = [
+  ['sunglasses', 1],
+  ['bags', 2]
+];
+checkInventory(order)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
