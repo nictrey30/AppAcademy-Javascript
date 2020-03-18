0a66ac8568ba21b07b9a27932dde4d61716cd718
Traian <niculescu.traian.ct@gmail.com>
2020-03-18 20:29:25 +0200

    update

--- codecademy/async-await/app1.js
+++ codecademy/async-await/app1.js
@@ -1,3 +1,9 @@
 function withConstructor(num) {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    if (num === 0) {
+      resolve('zero');
+    } else {
+      resolve('not zero');
+    }
+  });
 }
